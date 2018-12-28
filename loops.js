677f9daead4a2eaf0aba1ec3465e38d7865d8ba6
vyskoutaris <vyskoutaris@hotmail.com>
2018-12-28 15:26:48 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,4 +1,16 @@
 
-for (var i = 0; i < 25; i++) {
+function forLoop(array){
+for (var i = 0; i < 25; i++);
   console.log("I am ${i} strange loops.")
+}
+
+
+function whileLoop() {
+  return Math.random() >= 0.5; // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+ 
+// run until `maybeTrue()` returns `false`
+// (so the body of the loop might _never_ run!)
+while (maybeTrue()) {
+  console.log("And I ran; I ran so far away!");
 }
